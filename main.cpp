dae01a9d18ca8591316f2d2ec51ee03b6c924420
JohnN0022 <73184573+JohnN0022@users.noreply.github.com>
2022-10-26 13:19:15 -0400

    Update main.cpp
    Updated 10/26/22
    Added user have to enter integer in the age and id # or user will get error
    Added user info will be added to txt file

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 class Accounts{
 private:
-    char first_Name[40];
-    char last_Name[40];
+    char first_name[50];
+    char last_name[50];
     int std_id;
     int std_age;
-    int std_major;
+    char std_major[50];
     int user_input;
 public:
     //gathering info of student
@@ -22,25 +22,26 @@ public:
     void new_Account();
     void sign_in_Account(void);
     void lost_password(void);
+    void input_number(void);
 };
 void Accounts::getStd(void) {
     cout<<"Enter the student information"<<endl;
-    cout<<"Accounts name: ";cin>>first_Name;
-    cout<<"Accounts ID: ";cin>>std_id;
+    cout<<"Accounts name: ";cin >> first_name;
     cout<<"Accounts Age: ";cin>>std_age;
+    cout<<"Accounts ID: ";cin>>std_id;
     cout<<"Accounts Major: ";cin>>std_major;
 }
 void Accounts::display_std(void) {
     cout<<"The Accounts information"<<endl;
-    cout<<"Name: "<<first_Name<<endl;
+    cout << "Name: " << first_name << endl;
     cout<<"ID: "<<std_id<<endl;
     cout<<"Age: "<<std_age<<endl;
-    cout<<"Major: "<<std_major;
+    cout<<"Major: "<<std_major<<endl;
 }
-void Accounts::Title(void){
+void Accounts::Title(){
     cout<<"|_____Registration Title_____|"<<endl;
 }
-void Accounts::Menu(void){
+void Accounts::Menu(){
     cout<<"1) Make New Account"<<endl;
     cout<<"2) Sign in Account"<<endl;
     cout<<"3) Forgot Account Info"<<endl;
@@ -61,16 +62,32 @@ void Accounts::Menu(void){
     return Menu();
 }
 void Accounts::new_Account() {
+    cout<<"Registration Account Page"<<"\nPlease enter fill in your information"<<endl;
     fstream account_info;
-    cout<<"\nFirst Name: ";cin>>first_Name;
-    cout<<"\nLast Name: ";cin>>last_Name;
-    cout<<"\nWhat your age: ";cin>>std_age;
-
-    ofstream userName("A_username.txt");
+    //User Enter information
+    cout<<"\nFirst Name: ";cin >> first_name;
+    cout<<"\nLast Name: ";cin>>last_name;
+    input_number();
+    //cout<<"\nWhat your age: ";cin>>std_age;
+    cout<<"\nStudent ID: ";cin>>std_id;
+    cout<<"\nWhat is your major: "<<endl;cin>>std_major;
+    //Save user info into text file
+    ofstream userName("A_username.txt",ios::app);//ios::app wont delete/overwrite last entry
     if(userName.is_open()){
-        userName<<first_Name<<" "<<last_Name<<" "<<std_age;
+        userName << "Name:" << first_name << last_name << " " << "Age: " << std_age << " " << "Student ID: "
+        << std_id << " "<< "Major: " << std_major << endl;
         userName.close();
     }
+    display_std();
+    return sign_in_Account();
+}
+void Accounts::input_number() {
+    //user force to enter number and no char
+    while((cout<<"\nEnter your age: ") && (!(cin>>std_age)|| std_age < 1 || std_age > 110)){
+    cout<<"Invalid input, Try entering a number";
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
 }
 
 void Accounts::sign_in_Account() {
